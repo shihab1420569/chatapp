de7199fd24e4d26618f3ee2152e44556a40580eb
Shihab Rahman <1420569@iub.edu.bd>
2017-07-09 04:52:07 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+var app = require('express')();
+var http = require('http').Server(app);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(process.env.PORT, function(){
+  console.log('listening on *:3000');
+});
